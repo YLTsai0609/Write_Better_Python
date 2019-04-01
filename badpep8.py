4533421855f8dbc0d07068ee0f7ebc9ecd52748b
YLTsai0609 <yltsai0609@gmail.com>
2019-04-01 12:08:45 +0800

    clean the note

--- badpep8.py
+++ badpep8.py
@@ -5,12 +5,10 @@
 
 
 def foo_bar(arg1, arg2, arg3, arg4):
-    # way too much indentation
     return arg1, arg2, arg3, arg4
 
 
 def bar(*args):
-    # bad spacing
     return 2 + 2
 
 
@@ -26,14 +24,12 @@ class Treehouse:
         return 2
 
 
-# bad identation and whitespace
 alpha, beta, charlie, delta = foo_bar("a long string",
                                       "a longer string",
                                       "yet another long string",
                                       "and other crazy string")
 
 
-# bad spacing
 one = 1
 three = 3
 fourteen = 14  # make fourteen equal to 12
